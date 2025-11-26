e1742cea961bd21f42d557fd1a0bdae6cf7f15d4
samcrhs <samuelcampiriba@gmail.com>
2025-11-26 22:27:01 +0000

    canvis nou

--- src/my_robot_control/setup.py
+++ src/my_robot_control/setup.py
@@ -24,6 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'my_robot_lidar_test_exec = my_robot_control.my_robot_lidar_test:main',
+            'my_robot_lidar_test_1_exec = my_robot_control.my_robot_lidar_test_1:main',
             'my_robot_lidar_test_LIMO_exec = my_robot_control.my_robot_lidar_test_LIMO:main',
             'my_robot_control_exec = my_robot_control.my_robot_control:main',
             'my_robot_selfcontrol_exec = my_robot_control.my_robot_selfcontrol:main',
